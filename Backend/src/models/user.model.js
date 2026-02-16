8c7b6261e9f435f3e333436a5396685f14175eab
RajBhut <rajbhut2832005@gmail.com>
2026-02-17 01:21:29 +0530

    feat:department and sem in signup and fix git conflict

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,69 +1,56 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String },
-=======
 const userSchema = new mongoose.Schema({
-  name:String,
-  email:{type:String,unique:true,  required: true,},
-  password:String,
-  role:{type:String,enum:['student','faculty','parent','admin']},
-  linkedStudentId:String,
-   isVerified: { type: Boolean, default: false },
-   linkedFacultyId:String,
-   isVerified: { type: Boolean, default: false },
-  otp: Number,
-otpExpiry: Date
-})
->>>>>>> 334026d55b69dfe272dbc396ccf4647c0b1b2d26
+  name: { type: String },
 
-    email: { 
-      type: String, 
-      unique: true, 
-      required: true 
-    },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
 
-    password: { 
-      type: String, 
-      required: true 
-    },
+  password: {
+    type: String,
+    required: true,
+  },
 
-    role: { 
-      type: String, 
-      enum: ["student", "faculty", "parent", "admin"],
-      required: true
-    },
+  role: {
+    type: String,
+    enum: ["student", "faculty", "parent", "admin"],
+    required: true,
+  },
 
-    // 🔹 New Field (for students)
-    enrollmentNo: { 
-      type: String, 
-      unique: true, 
-      sparse: true 
-    },
+  department: {
+    type: String,
+  },
 
-   
-    linkedFacultyId: { 
-      type: String,
-       unique: true, 
-      sparse: true 
-    },
+  semester: {
+    type: Number,
+  },
 
-    // 🔹 OTP Verification
-    otp: { 
-      type: String 
-    },
+  enrollmentNo: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
 
-    otpExpiry: { 
-      type: Date 
-    },
+  linkedFacultyId: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
 
-    isVerified: { 
-      type: Boolean, 
-      default: false 
-    }
-  }
- 
-)
+  otp: {
+    type: String,
+  },
+
+  otpExpiry: {
+    type: Date,
+  },
+
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
 module.exports = mongoose.model("User", userSchema);
