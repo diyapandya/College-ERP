fc129d86507357f73de91965cc687bcc5bc55954
Diya <diyapandya1012@gmail.com>
2026-05-06 17:11:07 +0530

    Fix wildcard route using regex

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,17 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const rateLimit = require("./middleware/rateLimit.middleware");
 const path = require("path");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(rateLimit);
+
+// API routes
 app.use("/api", require("./routes"));
+
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
-// React routing support
-app.get("/*", (req, res) => {
+
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
 });
 
